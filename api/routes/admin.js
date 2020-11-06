bd706846c77b0a8af7412f9ad8d0f1127922c60d
Alejandra <sanchezlismairy@gmail.com>
2020-11-06 12:01:27 -0300

    modelos

--- api/routes/admin.js
+++ api/routes/admin.js
@@ -1,20 +1,4 @@
 const router = require("express").Router();
 const { Admin } = require("../db.js");
 
-router.post("/", (req, res) => {
-  const newAdmin = req.body;
-  Admin.create({
-    nombre: "admin",
-    apellifo: "admin",
-    DNI: 1236464,
-    email: "admin@henry.com",
-    password: "henry",
-  });
-  if (err) {
-    console.log(err);
-    return;
-  }
-  res.status(200).json({ msg: "OK", student: newStudent });
-});
-
 module.exports = router;
