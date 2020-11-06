0af696f565a179127834c970159dce6d06b5e800
Alejandra <sanchezlismairy@gmail.com>
2020-11-06 00:11:12 -0300

    modelo admin

---
+++ api/models/admin.js
@@ -0,0 +1,31 @@
+const mongoose = require("mongoose");
+
+const instructorSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  lastName: {
+    type: String,
+    required: true,
+  },
+  DNI: {
+    type: Number,
+    required: true,
+    unique: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+
+  password: {
+    type: String,
+    required: true,
+  },
+});
+
+const Instructor = mongoose.model("Instructor", instructorSchema);
+
+module.exports = Instructor;
