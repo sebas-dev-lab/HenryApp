936ce7ff524f40ed04a86eb740e1688c728e564f
cescontreras <contreras-cesaralejandro@hotmail.com>
2020-11-08 15:51:59 -0300

    rutas admin

--- api/routes/admin.js
+++ api/routes/admin.js
@@ -1,4 +1,38 @@
 const router = require("express").Router();
-const { Admin } = require("../db.js");
+const Admin = require("../models/admin");
+
+router.post("/", (req, res) => {
+  //const { name, lastName, Dni, email, password } = req.body;
+  
+  const admin = {
+    name: "admin",
+    lastName: "admin",
+    DNI: 123456,
+    email: "admin@aasdasdsds.com",
+    password: "henry",
+  }
+
+  Admin.create(admin, function (err, newAdmin) {
+		if (err) {
+			console.log(err);
+			return;
+		}
+		res.status(200).json({ msg: "OK", admin: newAdmin });
+	});
+});
+
+router.get("/all", (req, res) => {
+  Admin.find(function (err, admins) {
+    if (err) return console.error(err);
+    return admins;
+  })
+    .then((admins) => {
+      res.status(200).send(admins);
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+});
+
 
 module.exports = router;
--- api/routes/index.js
+++ api/routes/index.js
@@ -2,7 +2,9 @@ const express = require("express");
 const router = express();
 const studentRoutes = require("./student");
 const cohortRoutes = require("./cohort");
+const adminRoutes = require("./admin");
 
+router.use("/admin", adminRoutes);
 router.use("/cohort", cohortRoutes);
 router.use("/student", studentRoutes);
 
