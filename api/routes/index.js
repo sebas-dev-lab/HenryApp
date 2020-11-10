f699667ce416d1dd40471f48e7594db89786ac45
cescontreras <contreras-cesaralejandro@hotmail.com>
2020-11-10 12:21:48 -0300

    rutas grupo

--- api/routes/index.js
+++ api/routes/index.js
@@ -4,10 +4,14 @@ const studentRoutes = require("./student");
 const cohortRoutes = require("./cohort");
 const adminRoutes = require("./admin");
 const PpRoutes = require('./pair-programming')
+const groupRoutes = require("./group");
+
 
 router.use("/admin", adminRoutes);
 router.use("/cohort", cohortRoutes);
 router.use("/student", studentRoutes);
 router.use('/PP', PpRoutes);
+router.use("/group", groupRoutes);
+
 
 module.exports = router;
--- api/routes/student.js
+++ api/routes/student.js
@@ -3,12 +3,14 @@ const router = express();
 
 const Student = require("../models/student");
 const Cohort = require("../models/cohort");
+const Group = require("../models/group");
 
 router.get("/all", (req, res) => {
 	Student.find(function (err, students) {
 		if (err) return console.error(err);
 	})
 		.populate("idCohorte")
+		.populate("idGroup")
 		.then((students) => {
 			res.status(200).send(students);
 		})
@@ -71,5 +73,39 @@ router.put("/cohort/:student/:cohort", (req, res) => {
 		);
 	});
 });
+
+router.put("/group/:student/:group", (req, res) => {
+	const { student, group } = req.params;
+	//asignar grupo al estudiante
+	//sumar el estudiante al array del grupo
+	Student.findOne({ name: student }, function(err, findStudent){	
+		if(err){
+			res.status(400)
+			return
+		}
+		Group.findOneAndUpdate(
+			{ name: group }, 
+			{ $push: { students: findStudent }}, 
+			{new: true}
+		)
+			.then(() => {				
+				Group.findOne({ name: group }, function(err, group){
+					if(err){
+						res.status(400)
+						return
+					}
+					Student.findOneAndUpdate(
+						{ name: student }, 
+						{ $set: { idGroup: group }}, 
+						{new: true}
+					)	
+						.then(() => {
+							res.status(200).json({msg: "Ok"})
+						})
+				})
+			})				
+	})
+})
+			
 
 module.exports = router;
