490658c3b480f0f00066de37fa25e2521ac5149d
Alejandra <sanchezlismairy@gmail.com>
2020-11-10 17:53:29 -0300

    Google Funcionando

--- api/routes/auth.js
+++ api/routes/auth.js
@@ -6,4 +6,20 @@ router.post("/login", passport.authenticate("local"), (req, res) => {
   res.send(req.user);
 });
 
+//--------- Autenticación Google -----------
+
+router.get(
+  "/google",
+  passport.authenticate("google", {
+    scope: ["email", "profile"],
+  })
+);
+router.get(
+  "/google/callback",
+  passport.authenticate("google", {
+    successRedirect: "http://localhost:3000/",
+    session: false,
+  })
+);
+
 module.exports = router;
