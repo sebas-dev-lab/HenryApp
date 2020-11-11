3a88ce16b8eb7484f35c0c76e87edd5d1f2a3ce5
Alejandra <sanchezlismairy@gmail.com>
2020-11-11 02:19:11 -0300

    Componente sin Css

--- clients/src/App.js
+++ clients/src/App.js
@@ -1,42 +1,40 @@
-import React from 'react';
-import './App.css';
-import Home from './Componentes/Home';
-import Navbar from './Componentes/Navbar';
-import { Route, Switch, Link } from 'react-router-dom';
+import React from "react";
+import "./App.css";
+import Home from "./Componentes/Home";
+import Navbar from "./Componentes/Navbar";
+import Email from "./Componentes/Email";
+import { Route, Switch, Link } from "react-router-dom";
 
 function App() {
   return (
     <div>
       <Switch>
         {/* HOME */}
-        <Route path='/' exact>
+        <Route path="/" exact>
           <Home></Home>
         </Route>
         {/* HOME */}
 
         {/* ALUMNOS */}
-        <Route path='/alumnos' exact>
+        <Route path="/alumnos" exact>
           <Navbar></Navbar>
         </Route>
         {/* ALUMNOS */}
 
-         {/* INSTRUCTOR */}
-         <Route path='/instructor' exact>
+        {/* INSTRUCTOR */}
+        <Route path="/instructor" exact>
           <Navbar></Navbar>
         </Route>
         {/* INSTRUCTOR */}
 
-         {/* ADMIN */}
-         <Route path='/admin' exact>
+        {/* ADMIN */}
+        <Route path="/admin" exact>
           <Navbar></Navbar>
-          
+          <Email></Email>
         </Route>
         {/* ADMIN */}
       </Switch>
     </div>
-
-  
-    
   );
 }
 
--- clients/src/Componentes/Email.jsx
+++ clients/src/Componentes/Email.jsx
@@ -8,66 +8,164 @@ import {
   TableHead,
   TableRow,
   Modal,
+  Fab,
   Button,
-  TexField,
+  TextField,
 } from "@material-ui/core";
-import { Edit, Delete } from "@material-ui/icons";
-import bodyInsertar from "./Modal";
+import { Edit, Delete, Add } from "@material-ui/icons";
+// import styles from "../styles/email.module.css";
 import Axios from "axios";
 
-export default function Emails() {
-  const classes = useStyles();
+const useStyles = makeStyles((theme) => ({
+  root: {
+    height: 300,
+    flexGrow: 1,
+    minWidth: 300,
+    transform: "translateZ(0)",
+    "@media all and (-ms-high-contrast: none)": {
+      display: "none",
+    },
+  },
+  modal: {
+    position: "absolute",
+    width: 300,
+    backgroundColor: theme.palette.common.white,
+    border: "2px solid #000",
+    boxShadow: theme.shadows[5],
+    padding: theme.spacing(2, 4, 3),
+    top: "50%",
+    left: "50px",
+    transform: "translate (-50%, -50%)",
+  },
+  iconos: {
+    cursor: "pointer",
+  },
+  inputMaterial: {
+    width: "100%",
+  },
+}));
 
+export default function Emails() {
+  const styles = useStyles();
   //-------Hooks----
   const [data, setData] = useState([]);
   const [modalInsertar, setModalInsertar] = useState(false);
-
-  useEffect(() => {
-    Axios.get("http://localhost:3001/email/all").then((response) => {
+  const [modalEditar, setModalEditar] = useState(false);
+  const [emailSeleccionado, setEmailSeleccionado] = useState({
+    email: "",
+  });
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setEmailSeleccionado((prevState) => ({
+      ...prevState,
+      [name]: value,
+    }));
+  };
+  useEffect(async () => {
+    //prettier-ignore
+    await Axios.get("http://localhost:3001/email/all")
+    .then(response => {
       setData(response.data);
     });
   }, []);
+
+  const Aceptar = async () => {
+    //prettier-ignore
+    await Axios.post(
+      "http://localhost:3001/email/create",
+      emailSeleccionado
+    ).then(response => {
+      setData(data.concat(response.data))
+       AbrirCerrar();
+    });
+  };
 
   //Funcion para abrir y cerrar el modal
   const AbrirCerrar = () => {
     setModalInsertar(!modalInsertar);
   };
 
-  return (
-    <div className="Emails">
+  //Funcion para abrir y cerrar el modal al Editar
+  const Editar = () => {
+    setModalEditar(!modalEditar);
+  };
+
+  const bodyInsertar = (
+    <div className={styles.modal}>
+      <h3>Agregar Nuevo email</h3>
+      <TextField
+        className={styles.inputMaterial}
+        label="Email"
+        name="email"
+        onChange={handleChange}
+      />
       <br />
-      <Button onClick={AbrirCerrar}>Agregar</Button>
+      <br />
+      <div aling="right">
+        <Button variant="contained" color="secondary" onClick={Aceptar}>
+          Aceptar
+        </Button>
+        <Button variant="contained" color="primary" onClick={AbrirCerrar}>
+          Cancelar
+        </Button>
+      </div>
+    </div>
+  );
+  const bodyEditar = (
+    <div className={styles.modal}>
+      <h3>Editar email</h3>
+      <TextField
+        className={styles.inputMaterial}
+        label="Email"
+        name="email"
+        onChange={handleChange}
+        value={emailSeleccionado && emailSeleccionado.email}
+      />
+      <br />
+      <br />
+      <div aling="right">
+        <Button color="primary">Ediatr</Button>
+        <Button color="primary">Cancelar</Button>
+      </div>
+    </div>
+  );
+  return (
+    <div className="emails">
+      <br />
+      <Fab color="primary" aria-label="add" onClick={AbrirCerrar}>
+        <Add />
+      </Fab>
       <br />
       <br />
       <TableContainer>
-        <Table className={classes.table} aria-label="customized table">
+        <Table className={styles.table} aria-label="customized table">
           <TableHead>
             <TableRow>
-              <StyledTableCell>Invitación a HenryApp</StyledTableCell>
-              <StyledTableCell align="right">Email</StyledTableCell>
-              <StyledTableCell align="right">Acciones</StyledTableCell>
+              <TableCell align="left">Email</TableCell>
+              <TableCell align="right">Acciones</TableCell>
             </TableRow>
           </TableHead>
           <TableBody>
-            {data.map((consola) => (
-              <StyledTableRow key={consola.id}>
-                <StyledTableCell component="th" scope="row">
-                  {consola.email}
-                </StyledTableCell>
-                <StyledTableCell component="th" scope="row">
-                  <Edit />
+            {data.map((newEmail) => (
+              <TableRow key={newEmail.id}>
+                <TableCell component="th" scope="row">
+                  {newEmail.email}
+                </TableCell>
+                <TableCell component="th" scope="row">
+                  <Edit className={styles.iconos} />
                   &nbsp; &nbsp; &nbsp;
-                  <Delete />
-                </StyledTableCell>
-              </StyledTableRow>
+                  <Delete className={styles.iconos} />
+                </TableCell>
+              </TableRow>
             ))}
           </TableBody>
         </Table>
       </TableContainer>
-      <Modal>
-        open={modalInsertar}
-        onClose={AbrirCerrar}
-        {<bodyInsertar />}
+      <Modal open={modalInsertar} onClose={AbrirCerrar}>
+        {bodyInsertar}
+      </Modal>
+      <Modal open={modalEditar} onClose={Editar}>
+        {bodyEditar}
       </Modal>
     </div>
   );
