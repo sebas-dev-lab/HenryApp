ef6442fa865f34cc3a1c1c165099fdb18200834b
alezdut <alejandrozdut@gmail.com>
2020-11-11 21:56:01 -0300

    agregado login probare

--- clients/src/App.js
+++ clients/src/App.js
@@ -4,12 +4,19 @@ import Home from './Componentes/Home';
 import Navbar from './Componentes/Navbar';
 import { Route, Switch, Link } from 'react-router-dom';
 import Login from './Componentes/Login.jsx';
+import Login2 from "./Componentes/login/Login2.jsx"
 import BotonChat from './Componentes/Chat/BotonChat';
 
 function App() {
   return (
     <div>
       <Switch>
+        {/* login2 */}
+        <Route path='/login2' exact>
+          <Login2 />
+        </Route>
+        {/* login2 */}
+
         {/* HOME */}
         <Route path='/' exact>
           <Home></Home>
