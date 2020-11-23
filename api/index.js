d3610b036780cd47760aaff6f8c5ce801189b1a9
fayser17 <lb.lescano.vs@gmail.com>
2020-11-23 13:28:18 -0300

    agregado atlas db

--- api/index.js
+++ api/index.js
@@ -2,8 +2,12 @@ const app = require("./app");
 const mongoose = require("mongoose");
 const User = require("./models/user");
 
+const database_atlas =
+  "mongodb+srv://fayser:Vsl17477@cluster0.bucqc.mongodb.net/<dbname>?retryWrites=true&w=majority";
+const database_localhost = "mongodb://localhost/HenryApp";
+
 mongoose
-  .connect("mongodb://localhost/HenryApp", {
+  .connect(database_atlas, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
     useCreateIndex: true,
@@ -22,30 +26,31 @@ db.once("open", function () {
 });
 
 //--------------------------------SuperUser
-const bcrypt = require('bcryptjs');
+const bcrypt = require("bcryptjs");
 
 User.findOneAndUpdate(
-	{ name: "admin" }, // find a document with that filter
-	{
-		name: "admin",
-		email: "admin@henry.com",
-		role: "admin",
-	  password: bcrypt.hashSync("admin", 10),
-	}, // document to insert when nothing was found
-	{ upsert: true, new: true, runValidators: true }, // options
-	function (err, admin) { // callback
-			if (err) {
-				console.log(err);	
-			}else {
-				console.log(
-					"\n----Super-user---- \n #name: ",
-					admin.name,
-					"\n #email: ",
-					admin.email,
-					"\n #password: ",
-					admin.password,
-					"\n -----------------\n"
-				)				
-			}
-	}
+  { name: "admin" }, // find a document with that filter
+  {
+    name: "admin",
+    email: "admin@henry.com",
+    role: "admin",
+    password: bcrypt.hashSync("admin", 10),
+  }, // document to insert when nothing was found
+  { upsert: true, new: true, runValidators: true }, // options
+  function (err, admin) {
+    // callback
+    if (err) {
+      console.log(err);
+    } else {
+      console.log(
+        "\n----Super-user---- \n #name: ",
+        admin.name,
+        "\n #email: ",
+        admin.email,
+        "\n #password: ",
+        admin.password,
+        "\n -----------------\n"
+      );
+    }
+  }
 );
