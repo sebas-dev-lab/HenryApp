b7abbafd1ad463faa398fa9dbde8320d004c9a53
Alejandra <sanchezlismairy@gmail.com>
2020-11-25 12:50:51 -0300

    Funciona

--- api/index.js
+++ api/index.js
@@ -3,49 +3,53 @@ const mongoose = require("mongoose");
 const User = require("./models/user");
 
 mongoose
-  .connect("mongodb://localhost/HenryApp", {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-    useCreateIndex: true,
-    useFindAndModify: false,
-  })
-  .then(() => {
-    app.listen(app.get("port"), () => {
-      console.log(`Server on port ${app.get("port")}`);
+    .connect("mongodb://localhost/HenryApp", {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+        useCreateIndex: true,
+        useFindAndModify: false,
+    })
+    .then(() => {
+        app.listen(app.get("port"), () => {
+            console.log(`Server on port ${app.get("port")}`);
+        });
     });
-  });
 const db = mongoose.connection;
 
 db.on("error", console.error.bind(console, "connection error:"));
-db.once("open", function () {
-  console.log("DB connected");
+db.once("open", function() {
+    console.log("DB connected");
 });
 
 //--------------------------------SuperUser
 const bcrypt = require('bcryptjs');
 
-User.findOneAndUpdate(
-	{ name: "admin" }, // find a document with that filter
-	{
-		name: "admin",
-		email: "admin@henry.com",
-		role: "admin",
-	  password: bcrypt.hashSync("admin", 10),
-	}, // document to insert when nothing was found
-	{ upsert: true, new: true, runValidators: true }, // options
-	function (err, admin) { // callback
-			if (err) {
-				console.log(err);	
-			}else {
-				console.log(
-					"\n----Super-user---- \n #name: ",
-					admin.name,
-					"\n #email: ",
-					admin.email,
-					"\n #password: ",
-					admin.password,
-					"\n -----------------\n"
-				)				
-			}
-	}
+User.findOneAndUpdate({ name: "admin" }, // find a document with that filter
+    {
+        name: "admin",
+        email: "admin@henry.com",
+        role: "admin",
+        dni: 000000,
+        password: bcrypt.hashSync("admin", 10),
+    }, // document to insert when nothing was found
+    { upsert: true, new: true, runValidators: true }, // options
+    function(err, admin) { // callback
+        if (err) {
+            console.log(err);
+        } else {
+            console.log(
+                "\n----Super-user---- \n #name: ",
+                admin.name,
+                "\n #email: ",
+                admin.email,
+                "\n #password: ",
+                admin.password,
+                "\n #dni: ",
+                admin.dni,
+                "\n -----------------\n",
+
+
+            )
+        }
+    }
 );
--- clients/src/Componentes/login/Login2.jsx
+++ clients/src/Componentes/login/Login2.jsx
@@ -1,65 +1,109 @@
 import React, { useEffect, useState } from "react";
-import { useDispatch, useSelector } from 'react-redux';
+import { useDispatch, useSelector } from "react-redux";
 import "./Login2.css";
-import { authLogin } from '../../redux/actions/authActions';
-import { useHistory } from  'react-router-dom';
+import { authLogin } from "../../redux/actions/authActions";
+import { useHistory } from "react-router-dom";
+import GitHubIcon from '@material-ui/icons/GitHub'
+import GoogleIcon from '../utils/google.ico'
 
 
-export default function Login() { 
+export default function Login() {
+  const userLogin = useSelector((store) => store.auth.user.user);
+  console.log(userLogin, "*********************************");
 
-    const userLogin = useSelector(store => store.auth.user.user)
-    console.log(userLogin, "*********************************")
+  const [user, setUser] = useState("");
+  const [pass, setPass] = useState("");
+  const dispatch = useDispatch();
+  const history = useHistory();
 
-    const [user, setUser] = useState('');
-    const [pass, setPass] = useState('');
-    const dispatch = useDispatch();
-    const history = useHistory();
+  const userchange = (e) => {
+    setUser(e.target.value);
+  };
 
-    const userchange = (e) => {
-        setUser(e.target.value)
+  const passchange = (e) => {
+    setPass(e.target.value);
+  };
+
+  const enviar = async (e) => {
+    e.preventDefault();
+    await dispatch(authLogin(user, pass));
+  };
+
+  useEffect(() => {
+    if (userLogin && userLogin.role === "admin") {
+      history.push("/admin");
     }
-
-    const passchange = (e) => {
-        setPass(e.target.value)
+    if (userLogin && userLogin.role === "student") {
+      history.push("/alumnos");
     }
+  }, [userLogin]);
 
-    const enviar = async (e) => {
-        e.preventDefault()
-        await dispatch(authLogin(user, pass))
+  return (
+    <div className="align">
+      <div className="grid">
+        <form className="form login">
+          <div className="form__field">
+            <label for="login__username">
+              <span className="hidden">Usuario</span>
+            </label>
+            <input
+              onChange={userchange}
+              id="login__username"
+              type="text"
+              name="username"
+              className="form__input"
+              placeholder="Correo"
+              required
+            />
+          </div>
+
+          <div class="form__field">
+            <label for="login__password">
+              <span className="hidden">Contraseña</span>
+            </label>
+            <input
+              onChange={passchange}
+              id="login__password"
+              type="password"
+              name="password"
+              className="form__input"
+              placeholder="Contraseña"
+              required
+            />
+          </div>
+
+          <div className="form__field" onClick={enviar}>
+            <input type="submit" value="Ingresar" />
+          </div>
+
+          {/* Boton Facebook  */}
+          <a href="http://localhost:3001/auth/github" >
+            <div
+              className="btn-alternativo"
+              id="github"
+              value="github"
+            >
+                {/* <GitHubIcon/> */}
+                <img src="https://image.flaticon.com/icons/png/512/25/25231.png" />
+                <p>Continuar con GitHub</p>
          
-    }
-
-    useEffect(()=>{
-        if(userLogin && userLogin.role === 'admin'){
-            history.push('/admin') 
-        }
-        if(userLogin && userLogin.role === 'student'){
-            history.push('/alumnos') 
-        }
-    }, [userLogin])
-
-    return (
-        <div className="align">
-
-            <div className="grid">
-
-                <form className="form login">
-
-                    <div className="form__field">
-                        <label for="login__username"><span className="hidden">Usuario</span></label>
-                        <input onChange={userchange} id="login__username" type="text" name="username" className="form__input" placeholder="Correo" required />
-                    </div>
-
-                    <div class="form__field">
-                        <label for="login__password"><span className="hidden">Contraseña</span></label>
-                        <input onChange={passchange} id="login__password" type="password" name="password" className="form__input" placeholder="Contraseña" required />
-                    </div>
-
-                    <div className="form__field" onClick={enviar}>
-                        <input type="submit" value="Ingresar" />
-                    </div>
-                </form>
             </div>
-        </div>
-    )
+          </a>
+
+          {/* Boton Google */}
+          <a href="http://localhost:3001/auth/google">
+            <div
+              className="btn-alternativo"
+              id="goog"
+              value="google"
+            >
+                <img src="https://cdn.icon-icons.com/icons2/673/PNG/128/Google_icon-icons.com_60497.png"/>
+              <p>CONTINUAR CON GOOGLE</p>
+                
+    </div>
+          </a>
+        </form>
+      </div>
+    </div>
+  );
 }
