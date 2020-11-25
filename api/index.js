1e313f7172de8abca53450eadec1f6adef7016a5
fayser17 <lb.lescano.vs@gmail.com>
2020-11-25 15:28:10 -0300

    conflictos solucionados

--- api/index.js
+++ api/index.js
@@ -17,42 +17,44 @@ mongoose
     app.listen(app.get("port"), () => {
       console.log(`Server on port ${app.get("port")}`);
     });
+  });
+
 const db = mongoose.connection;
 
 db.on("error", console.error.bind(console, "connection error:"));
-db.once("open", function() {
-    console.log("DB connected");
+db.once("open", function () {
+  console.log("DB connected");
 });
 
 //--------------------------------SuperUser
 const bcrypt = require("bcryptjs");
 
-User.findOneAndUpdate({ name: "admin" }, // find a document with that filter
-    {
-        name: "admin",
-        email: "admin@henry.com",
-        role: "admin",
-        dni: 000000,
-        password: bcrypt.hashSync("admin", 10),
-    }, // document to insert when nothing was found
-    { upsert: true, new: true, runValidators: true }, // options
-    function(err, admin) { // callback
-        if (err) {
-            console.log(err);
-        } else {
-            console.log(
-                "\n----Super-user---- \n #name: ",
-                admin.name,
-                "\n #email: ",
-                admin.email,
-                "\n #password: ",
-                admin.password,
-                "\n #dni: ",
-                admin.dni,
-                "\n -----------------\n",
-
-
-            )
-        }
+User.findOneAndUpdate(
+  { name: "admin" }, // find a document with that filter
+  {
+    name: "admin",
+    email: "admin@henry.com",
+    role: "admin",
+    dni: 000000,
+    password: bcrypt.hashSync("admin", 10),
+  }, // document to insert when nothing was found
+  { upsert: true, new: true, runValidators: true }, // options
+  function (err, admin) {
+    // callback
+    if (err) {
+      console.log(err);
+    } else {
+      console.log(
+        "\n----Super-user---- \n #name: ",
+        admin.name,
+        "\n #email: ",
+        admin.email,
+        "\n #password: ",
+        admin.password,
+        "\n #dni: ",
+        admin.dni,
+        "\n -----------------\n"
+      );
     }
+  }
 );
