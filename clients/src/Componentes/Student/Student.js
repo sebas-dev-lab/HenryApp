1bfafee87cca6fec507fd8e3ca182c9fd160ffe8
unknown <nailavanessa2009_@hotmail.com>
2020-11-25 10:52:54 -0600

    wwww

--- clients/src/Componentes/Student/Student.js
+++ clients/src/Componentes/Student/Student.js
@@ -1,4 +1,7 @@
+import React, { useEffect } from "react";
 import { Link } from "react-router-dom";
+import { useSelector, useDispatch } from "react-redux";
+
 // import {logoutUser} from '..Redux/u';
 // import "./Components/app.css";
 // import img1 from "./img/pexels-photo-392018.jpeg"
@@ -15,121 +18,122 @@ import calif from "../utils/calif.jpg";
 import Footer from "../../Componentes/Footer.jsx";
 import Carrusel from "../Carrusel.jsx";
 import Stepper from "../../Componentes/Stepper/Stepper";
+import Navbar from "../Navbar";
+import BotonChat from "../Chat/BotonChat";
+import { verifySession } from "../../redux/actions/authActions";
 
 const Student = () => {
+  const { user } = useSelector((store) => store.auth);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(verifySession());
+  }, []);
 
   return (
-    <div class="root">
-      <Carrusel />
-      <div className="container">
-        <Stepper />
-      </div>
-
-      <div className="algo">
-        <div className="conten_taks">
-            
-              <div class="category-box">
-                <img key={img3} src={img3} alt="" />
-                <div className="cont_oscuro">
-                <div class="content">
-                  <div>
-                    <h2>Calendario</h2>
-                    <div className="boton_link">
-                    <Link className="link" to="/calendario">Ingresar</Link>
-                    </div>
+    <>
+      <Navbar user={user} />
+      <div class="root">
+        <Carrusel />
+        <div className="container">
+          <Stepper />
+        </div>
+        <div className="algo">
+          <div className="conten_taks">
+            <div class="category-box">
+              <img key={img3} src={img3} alt="" />
+              <div class="content">
+                <div>
+                  <h2>Calendario</h2>
+                  <div className="boton_link">
+                    <Link className="link" to="/calendario">
+                      Ingresar
+                    </Link>
                   </div>
                 </div>
               </div>
             </div>
-        
-          <div class="category-box">
-            <img key={img4} src={img4} alt="" />
-            <div className="cont_oscuro">
-            <div class="content">
-              <div>
-                <h2>Acceso a clase</h2>
-                <div className="boton_link">
-                  <Link className="link">Ingresar</Link>
-                </div>
-              </div>
-            </div>
-            </div>
-          </div>
 
-          <div class="category-box">
-            <img key={img2} src={img2} alt="" />
-            <div className="cont_oscuro">
-            <div class="content">
-              <div>
-                <h2>Homeworks</h2>
-                <div className="boton_link">
-                  <Link className="link">Ingresar</Link>
+            <div class="category-box">
+              <img key={img4} src={img4} alt="" />
+              <div class="content">
+                <div>
+                  <h2>Acceso a clase</h2>
+                  <div className="boton_link">
+                    <Link className="link">Ingresar</Link>
+                  </div>
                 </div>
               </div>
             </div>
+            <div class="category-box">
+              <img key={img2} src={img2} alt="" />
+              <div class="content">
+                <div>
+                  <h2>Homeworks</h2>
+                  <div className="boton_link">
+                    <Link className="link">Ingresar</Link>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="conten_taks">
+            <div class="category-box">
+              <img key={coding} src={coding} alt="" />
+              <div class="content">
+                <div>
+                  <h2>Clases grabadas</h2>
+                  <div className="boton_link">
+                    <Link className="link" to="/clases">
+                      Ingresar
+                    </Link>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div class="category-box">
+              <img key={meeting} src={meeting} alt="" />
+              <div class="content">
+                <div>
+                  <h2>Stand Up</h2>
+                  <div className="boton_link">
+                    <Link className="link">Ingresar</Link>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div class="category-box">
+              <img key={pairp} src={pairp} alt="" />
+              <div class="content">
+                <div>
+                  <h2>Pair Programming</h2>
+                  <div className="boton_link">
+                    <Link className="link">Ingresar</Link>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+          {/* <div style={{ marginLeft: "50px"}} >   */}
+          <div className="conten_taks">
+            <div class="category-box">
+              <img key={calif} src={calif} alt="" />
+              <div class="content">
+                <div>
+                  <h2>Calificaciones</h2>
+                  <div className="boton_link">
+                    <Link className="link">Ingresar</Link>
+                  </div>
+                </div>
+              </div>
             </div>
           </div>
         </div>
-        <div className="conten_taks">
-          <div class="category-box">
-            <img key={coding} src={coding} alt="" />
-            <div className="cont_oscuro">
-            <div class="content">
-              <div>
-                <h2>Clases grabadas</h2>
-                <div className="boton_link">
-                  <Link className="link" to="/clases">Ingresar</Link>
-                </div>
-              </div>
-            </div>
-            </div>
-          </div>
-          <div class="category-box">
-            <img key={pairp} src={pairp} alt="" />
-            <div className="cont_oscuro">
-            <div class="content">
-              <div>
-                <h2>Stand Up</h2>
-                <div className="boton_link">
-                  <Link className="link">Ingresar</Link>
-                </div>
-              </div>
-            </div>
-            </div>
-          </div>
-          <div class="category-box">
-            <img key={meeting} src={meeting} alt="" />
-            <div className="cont_oscuro">
-            <div class="content">
-              <div>
-                <h2>Pair Programming</h2>
-                <div className="boton_link">
-                  <Link className="link">Ingresar</Link>
-                </div>
-              </div>
-            </div>
-            </div>
-          </div>
-        </div>
-        {/* <div style={{ marginLeft: "50px"}} >   */}
-        <div className="conten_taks">
-          <div class="category-box">
-            <img key={calif} src={calif} alt="" />
-            <div className="cont_oscuro">
-            <div class="content">
-              <div>
-                <h2>Calificaciones</h2>
-                <div className="boton_link">
-                  <Link className="link">Ingresar</Link>
-                </div>
-              </div>
-            </div>
-            </div>
-          </div>
-        </div>
+        <BotonChat />
       </div>
+
       <Footer />
-    </div>
+    </>
   );
 };
 
