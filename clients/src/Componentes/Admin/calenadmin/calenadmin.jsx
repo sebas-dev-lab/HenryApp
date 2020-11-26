6fcb4fe0cc85f58e0eb615c54d1377fd4b69794f
Santiago Barrios <santiagobarrios91@gmail.com>
2020-11-26 12:58:22 -0300

    cambios en calendar

--- clients/src/Componentes/Admin/calenadmin/calenadmin.jsx
+++ clients/src/Componentes/Admin/calenadmin/calenadmin.jsx
@@ -1,6 +1,9 @@
-import React from 'react';
+import { React, useState, useEffect} from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import TextField from '@material-ui/core/TextField';
+import { useDispatch } from "react-redux";
+import { postCalendar } from "../../../redux/actions/calendarActions";
+
 
 
 const useStyles = makeStyles((theme) => ({
@@ -22,7 +25,67 @@ const useStyles = makeStyles((theme) => ({
 }));
 
 export default function DateAndTimePickers() {
+  const [listener, setListener] = useState(false)
+  const [textFieldValue, setTextFieldValue] = useState('');
+  const [dateFieldValue, setDateFieldValue] = useState('');
+  const [endFieldValue, setEndFieldValue] = useState('');
   const classes = useStyles();
+  const dispatch = useDispatch();
+  
+
+  function handleDateFieldChange(e) {
+    setDateFieldValue(
+        e.target.value
+    )
+}
+
+function handleEndDateFieldChange(e) {
+  setEndFieldValue(
+    e.target.value
+  )
+}
+
+  function handleTitleFieldChange(e) {
+    setTextFieldValue(
+      e.target.value
+    )
+  }
+
+  function handleSubmit(){
+    setListener(true)
+  }
+
+ /*  title, year, month, day, hour, minute, endYear, 
+      endMonth, endDay, endHour, endMinute, allDay */
+  
+    useEffect(() => {
+      if(listener===true && textFieldValue && dateFieldValue && endFieldValue){
+        const AMZ = dateFieldValue.split('-')
+        const DFullH = AMZ[2].split('T')
+        const HM = DFullH[1].split(':')
+        const primero = endFieldValue.split('-')
+        const segundo = primero[2].split('T')
+        const tercero = segundo[1].split(':')
+        const event = {
+          title:textFieldValue,
+          year:AMZ[0],
+          month:AMZ[1],
+          day:DFullH[0],
+          hour:HM[0],
+          minute:HM[1],
+          endYear:primero[0],
+          endMonth:primero[1],
+          endDay:segundo[0],
+          endHour:tercero[0],
+          endMinute:tercero[1],
+          allDay:false
+          }
+        console.log('que evento???', event)
+        dispatch(postCalendar(event));
+      }
+      
+    }, [handleSubmit]);
+  
 
   return (
     <div>
@@ -30,20 +93,40 @@ export default function DateAndTimePickers() {
      
       <TextField
         id="datetime-local"
-        label="Next appointment"
+        label="Event Start"
         type="datetime-local"
-        defaultValue="2017-05-24T10:30"
+        /* defaultValue="2017-05-24T10:30" */
         className={classes.textField}
         InputLabelProps={{
           shrink: true,
         }}
+        value={dateFieldValue} 
+        onChange={handleDateFieldChange}
+      />
+    </form>
+
+    <form className={classes.container} noValidate>
+     
+      <TextField
+        id="datetime-local"
+        label="Event End"
+        type="datetime-local"
+        /* defaultValue="2017-05-24T10:30" */
+        className={classes.textField}
+        InputLabelProps={{
+          shrink: true,
+        }}
+        value={endFieldValue} 
+        onChange={handleEndDateFieldChange}
       />
     </form>
 
 
      <form className={classes.root} noValidate autoComplete="off">
-      <TextField id="outlined-basic" label="Outlined" variant="outlined" />
+      <TextField id="outlined-basic" label="Title" variant="outlined" value={textFieldValue} 
+        onChange={handleTitleFieldChange} />
     </form>
+    <button onClick={handleSubmit}>Submit</button>
     </div>
   );
 } //
